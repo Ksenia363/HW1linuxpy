3bcc396bc13ced83b43350774b1d7397ff26e880
Ksenia363 <123712591+Ksenia363@users.noreply.github.com>
2023-11-28 14:40:43 +0400

    Create HW1.py

---
+++ HW1.py
@@ -0,0 +1,8 @@
+import subprocess
+def verification(command, text):
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    out = result.stdout
+    if result.returncode == 0 and text in out:
+        print('TRUE')
+    else:
+        print('FALSE')
